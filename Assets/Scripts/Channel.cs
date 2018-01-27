9a27f31839767c4017bf04a5d8178a197d67e1fe
Jacob Golden-Needham <therealbunn1e5@gmail.com>
2018-01-27 02:27:23 -0800

    Changed interface structure

--- Assets/Scripts/Channel.cs
+++ Assets/Scripts/Channel.cs
@@ -2,9 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface Channel {
+public class Channel : IChannelBase{
+
+    public void Win() {
+
+    }
+
+    public void Lose() {
+
+    }
+}
+
+public interface IChannel : IChannelBase{
     void Generate();
     void CleanUp();
+}
+
+public interface IChannelBase {
     void Win();
     void Lose();
 }
