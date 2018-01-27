25095b893f989ba1ee60aa2e0426df10f514a7c3
Jacob Golden-Needham <therealbunn1e5@gmail.com>
2018-01-27 03:10:40 -0800

    added channel types

---
+++ Assets/Scripts/DanceChannel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceChannel : Channel, IChannel {
+
+    public void Generate() {
+
+    }
+
+    public void CleanUp() {
+
+    }
+
+}
---
+++ Assets/Scripts/PongChannel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PongChannel : Channel, IChannel{
+    public void Generate(){
+
+    }
+
+    public void CleanUp(){
+
+    }
+}
