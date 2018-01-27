997542f728e16609776de00a006c2ad7f229a26f
Jacob Golden-Needham <therealbunn1e5@gmail.com>
2018-01-27 03:10:02 -0800

    Added Channel types

--- Assets/Scripts/ChannelManager.cs
+++ Assets/Scripts/ChannelManager.cs
@@ -3,4 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ChannelManager : MonoBehaviour {
+
+    List<IChannel> channel = new List<IChannel>();
+
+    public ChannelManager() {
+
+    }
 }
