91f3f41e24f0dfaa59ca9f53186cd6fa37ae62dd
Jacob Golden-Needham <therealbunn1e5@gmail.com>
2018-01-28 11:32:55 -0800

    Fixed rotateHame, added bulletSpawn working on bullet logic

--- Assets/Scripts/BlockingGame.cs
+++ Assets/Scripts/BlockingGame.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BlockingGame : MonoBehaviour {
-
-    private GameObject screen;
+    
     public float sensitivity;
-    float position = 0;
+    public float min, max;
+    public float position;
+
     void Start() {
-        screen = GameObject.FindGameObjectWithTag("Screen");
     }
 
 	// Update is called once per frame
 	void Update () {
-        position = Mathf.Clamp(DialControl.Position(), -screen.transform.lossyScale.x / 2, screen.transform.lossyScale.x / 2);
+        position = DialControl.ClampedPosition(position, min, max);
         this.transform.position = Vector3.Lerp(this.transform.position, new Vector3(position, this.transform.position.y, this.transform.position.z), 6f * Time.deltaTime);
 	}
 }
