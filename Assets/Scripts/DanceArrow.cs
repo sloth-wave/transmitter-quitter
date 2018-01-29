c382bdf80d5f8da4361d8daded9c37e9c749492a
Karen <photo.karen@gmail.com>
2018-01-28 18:23:13 -0800

    Fix broken dance game

--- Assets/Scripts/DanceArrow.cs
+++ Assets/Scripts/DanceArrow.cs
@@ -6,10 +6,14 @@ public class DanceArrow : MonoBehaviour {
 
     Dance dance;
 
+    public void Awake()
+    {
+        dance = FindObjectOfType<Dance>();
+    }
+
     public void OnTriggerEnter2D (Collider2D other)
     {
         dance.GetComponent<Dance>();
         dance.Hit();
-        print("I got hit " + name);
     }
 }
