5aac16b19e8fe7f9b8dcbf9c671db1db7e776a30
Karen <photo.karen@gmail.com>
2018-01-28 18:23:51 -0800

    Add dancing assets and script

---
+++ Assets/Scripts/RotateArms.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateArms : MonoBehaviour {
+
+    public float scroll;
+    public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scroll = Input.GetAxis("Dial");
+
+        transform.Rotate(0, 0, scroll * speed);
+	}
+}
